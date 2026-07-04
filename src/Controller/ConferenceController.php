aca464208d08a966ccd6c09b1db88ba5188975e6
Symfonyaddict <formationsymfony5@gmail.com>
2026-07-04 16:56:40 +0200

    creation de l'entité conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,23 +2,18 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    #[Route('/',  name: 'homepage')]
+    public function index(Request $req): Response
     {
-        return new Response(<<<EOF
-           <html>
-                <body>
-                    <img src="/images/under-construction.gif" />                
-                </body>
-           </html>
-           EOF
-       );
+        return $this->render('conference/index.html.twig');
 
     }
 }
